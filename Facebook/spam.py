3ee53a1ebdf71862be7fcefd9f0a9db8c0544ed2
Drummer3 <57110641+Drummer3@users.noreply.github.com>
2020-07-13 16:58:42 +0400

    Facebook ChatBot

---
+++ Facebook/spam.py
@@ -0,0 +1,17 @@
+from fbchat import Client
+from fbchat.models import Message
+
+username = "+995591080888"
+password = "Longlivex"
+client = Client(username, password)
+#სალომეს id
+thread_id='100004441952367'
+
+# # # # # #
+
+msg = f"256 ხარისხად 256 = {256**256}"
+client.send(Message(text=msg), thread_id=thread_id)
+
+# # # # # #
+
+client.logout()
