4abbfad5cefe1c29acc1d6df8441522de0edaab5
Drummer3 <57110641+Drummer3@users.noreply.github.com>
2020-07-15 00:35:16 +0400

    Added things

---
+++ Fun/password generator.py
@@ -0,0 +1,18 @@
+#basic password generator
+
+import random
+
+#string of all available characters
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
+
+#asks user how long password to be
+length = input('password length? ')
+length = int(length)
+
+#generates it
+password = ''
+for i in range(length):
+    password += random.choice(chars)
+
+#prints it duh
+print(password)
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 #this is test for running another python file from this file
-
+#and it works
 import test2
-
-test = test2
+test2
