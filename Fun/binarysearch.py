de1a61d00a14975ed0324c67ed83089916566adf
Drummer3 <57110641+Drummer3@users.noreply.github.com>
2020-08-02 10:54:27 +0400

    Backup

--- Fun/binarysearch.py
+++ Fun/binarysearch.py
@@ -21,12 +21,12 @@ arr.sort()
 
 # choses random number in same range
 x = random.randint(0,100) 
-
+print(arr,x)
 # algorithm
 result = binarySearch(arr, 0, len(arr)-1, x)
 
 # checks result and prints it
 if result != -1:
-    print("X is in the list at index % d" % result)
+    print("X is in the list at index %d"% result)
 else:
     print("X isn't in the list")
--- Fun/drawing-on-x-y.py
+++ Fun/drawing-on-x-y.py
@@ -1,8 +1,7 @@
 # this is matplotlib 
 import matplotlib.pyplot as plt 
-
+plt.rcParams['toolbar'] = 'none'
 plt.plot([1, 2, 3, 4], [1, 4, 9, 16]) #[x1,x2,x3,x4][y1,y2,y3,y4]
 plt.xlabel('x label')
 plt.ylabel('y label')
-plt.title("title")
 plt.show()
