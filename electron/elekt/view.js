997f11c4231179d99720097c02e48f58a3798fa6
Drummer3 <57110641+Drummer3@users.noreply.github.com>
2020-08-22 23:59:04 +0400

    Studio Managment v0.1

---
+++ electron/elekt/view.js
@@ -0,0 +1,35 @@
+let $ = require('jquery')
+let fs = require('fs')
+let filename = 'contacts'
+
+
+function addEntry(date, name, quantity, from, till, price) {
+   if(date && name && quantity && from && till && price) {
+      let updateString = '<tr><td class="date">'+ date + '</td><td class="name">'+ name +'</td><td class="qnt">' 
+         + quantity +'</td><td class="from">' + from + '</td><td class="till">' + till +'</td><td class="price">'
+         + price +'</td></tr>'
+      $('#contact-table').append(updateString)
+   }
+}
+
+function loadAndDisplayContacts() {  
+   
+   //Check if file exists
+   if(fs.existsSync(filename)) {
+      let data = fs.readFileSync(filename, 'utf8').split('\n')
+      
+      data.forEach((row, index) => {
+         let [ date, name, quantity, from, till, price ] = row.split(',')
+         addEntry(date, name, quantity, from, till, price)
+      })
+   
+   } else {
+      console.log("File Doesn\'t Exist. Creating new file.")
+      fs.writeFile(filename, '', (err) => {
+         if(err)
+            console.log(err)
+      })
+   }
+}
+
+loadAndDisplayContacts()
