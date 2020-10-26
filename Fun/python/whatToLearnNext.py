dcccbd97d107d9f1f5413c55fd66cc157d8a0b53
Drummer3 <57110641+Drummer3@users.noreply.github.com>
2020-10-26 15:26:49 +0400

    added random generator for next learning ideas

---
+++ Fun/python/whatToLearnNext.py
@@ -0,0 +1,8 @@
+import random
+
+languages = ('Java','Node.JS','MongoDB','MySQL','ReactJS','JQuerry','C#','.NET','Flutter','Objective C',)
+
+learnthis = random.choice(languages)
+print("***")
+print(learnthis)
+print("***")
